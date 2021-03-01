d15117e8e2ad7cab16e8f02f2f4e2703568f19c1
Siily15 <arnokosk15@gmail.com>
2021-03-01 11:15:43 +0200

    new files

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+include 'header.php'
+$array = [
+     'Avaleht',
+     'Kontak',
+     'Meist'
+];
+
+foreach ($array as $value) {
+    echo '<span class=nav-item>' $value '</span>'
+}
+
+include 'footer.php'
